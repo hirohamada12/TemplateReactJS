5664cd83347a3a72453affd382d6a3232db0a54f
Phan Nhật Anh <nhatanhvt123@gmail.com>
2021-04-24 10:50:33 +0700

    Init

---
+++ config/proxy.js
@@ -0,0 +1,20 @@
+export default {
+  dev: {
+    '/api/': {
+      target: 'https://preview.pro.ant.design',
+      changeOrigin: true,
+      pathRewrite: {
+        '^': '',
+      },
+    },
+  },
+  pre: {
+    '/api/': {
+      target: 'your pre url',
+      changeOrigin: true,
+      pathRewrite: {
+        '^': '',
+      },
+    },
+  },
+};
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { useIntl } from 'umi';
+
+export default () => {
+  // eslint-disable-next-line no-unused-vars
+  const intl = useIntl();
+  return (
+    <PageContainer>
+
+    </PageContainer>
+  );
+};
